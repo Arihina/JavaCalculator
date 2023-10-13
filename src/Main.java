741f7d4eaf22ced3e541b22406322a199016994d
Alena Kozlova <alena.kozlova2004@mail.ru>
2023-10-13 21:35:56 +0300

    v-2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,28 @@
-public class Main {
+import java.util.Scanner;
+
+public class Main
+{
     public static void main(String[] args) {
-        int num1 = 1;
-        int num2 = 2;
-        int res = num1 * num2;
-        System.out.println("res = " + res);
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the first number");
+        double num1 = in.nextDouble();
+        System.out.println("Enter the second number");
+        double num2 = in.nextDouble();
+        System.out.println("Enter the operation");
+        String operation = in.next();
+        double result = calculate(num1, num2, operation);
+        System.out.println("Result = " + result);
+    }
+    public static double calculate(double num1, double num2, String operation){
+        double result;
+        switch (operation){
+            case "+": result = num1 + num2; break;
+            case "-": result = num1 - num2; break;
+            case "*": result = num1 * num2; break;
+            case "/": result = num1 / num2; break;
+            case "%": result = num1 % num2; break;
+            default: result = 0.0; break;
+        }
+        return result;
     }
 }
