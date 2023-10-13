87538752f468c71e4da8b35ccdf25d58b1ccc46e
Alena Kozlova <alena.kozlova2004@mail.ru>
2023-10-13 21:17:28 +0300

    v-1

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 1;
+        int num2 = 2;
+        int res = num1 * num2;
+        System.out.println("res = " + res);
+    }
+}
